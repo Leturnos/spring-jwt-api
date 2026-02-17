a5924d4ea74336974750a130c90cdab7c307b816
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2026-02-17 11:21:14 -0300

    feat(auth): implement custom UserDetailsService

--- src/main/java/io/github/leturnos/jwt_api/service/UserService.java
+++ src/main/java/io/github/leturnos/jwt_api/service/UserService.java
@@ -4,13 +4,18 @@ import io.github.leturnos.jwt_api.model.Role;
 import io.github.leturnos.jwt_api.model.User;
 import io.github.leturnos.jwt_api.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
     private final RoleService roleService;
@@ -29,5 +34,19 @@ public class UserService {
 
     public List<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow();
+
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+        // Full path to avoid repeated name
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), authorities);
     }
 }
